5311fba744009e9384611389d294be683e9b6bf5
Leiva Maximiliano <45923631+MaximilianoArg@users.noreply.github.com>
2025-11-08 15:56:40 -0300

    Se agrega lazy_loading para los distintos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,36 @@ import { PanelPrincipal } from './panel-principal/panel-principal';
 import { GuardiaAutenticacion } from './autenticar-guard';
 
 export const routes: Routes = [
-    { path: 'login', component: ComponenteLogueo },
+    /*{ path: 'login', component: ComponenteLogueo },
     { path: 'logout', component: Desloguear },
     { path: 'panel-principal', component: PanelPrincipal, canActivate: [GuardiaAutenticacion]},
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' }*/
+    // Nueva direcciones para las rutas..
+    { path: 'login', component: ComponenteLogueo },
+    { path: 'logout', component: Desloguear },
+    {
+        path: '',
+        component: PanelPrincipal,
+        canActivate: [GuardiaAutenticacion],
+        children: [
+            { path: 'panel_principal', loadComponent: () => import('./panel-principal/panel-principal').then(m => m.PanelPrincipal) },
+            {
+                path: 'ajustes',
+                loadChildren: () => import('./ruteo/ajustes.routes').then(m => m.AJUSTES_RUTAS)
+            },
+            {
+                path: 'desarrollo',
+                loadChildren: () => import('./ruteo/desarrollo.routes').then(m => m.DESARROLLO_RUTAS)
+            },
+            {
+                path: 'finanzas',
+                loadChildren: () => import('./ruteo/finanzas.routes').then(m => m.FINANZAS_RUTAS)
+            },
+            {
+                path: 'productividad',
+                loadChildren: () => import('./ruteo/productividad.routes').then(m => m.PRODUCTIVDAD_RUTAS)
+            },
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+        ]
+    },
 ];
---
+++ src/app/ruteo/productividad.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const PRODUCTIVDAD_RUTAS: Routes = [
+    {
+        path: 'inversiones',
+        children: [
+            { path: 'cartera', loadComponent: () => import('./inversiones/cartera/cartera.component').then(m => m.CarteraComponent) },
+            // ... otras rutas de inversiones
+        ]
+    },
+    {
+        path: 'balances',
+        children: [
+            { path: 'mensual', loadComponent: () => import('./balances/mensual/mensual.component').then(m => m.ResumenMensualComponent) },
+            // ... otras rutas de balances
+        ]
+    }
+];
